e02ec7b7f173b27cfcc0e2e163a82e72854ef971
alex_ust <alexust777888@gmail.com>
2026-01-16 01:41:42 +0200

    Add printing to the tests

--- integration_testing.py
+++ integration_testing.py
@@ -5,12 +5,19 @@ BASE_URL = os.environ.get("TEST_BASE_URL", "http://localhost:8080")
 
 def test_health():
     res = requests.get(f"{BASE_URL}/health", timeout=5)
+    print("test_health \033[92mOK\033[0m" if res.status_code == 200 else "\033[91mFailed\033[0m" )
     assert res.status_code == 200
 
 
 def test_db_query():
     create = requests.post(f"{BASE_URL}/api/tasks", json={"title": "integration_test", "details": "db check 💋"}, timeout=5)
+    print("test_db_query created task with ID:", create.json().get("id"), "\033[92mOK\033[0m" if create.status_code == 201 else "\033[91mFailed\033[0m" )
     assert create.status_code == 201
 
     items = requests.get(f"{BASE_URL}/api/tasks", timeout=10)
+    print("test_db_query fetched tasks \033[92mOK\033[0m" if items.status_code == 200 else "\033[91mFailed\033[0m" )
     assert items.status_code == 200
+
+    item_delete = requests.delete(f"{BASE_URL}/api/tasks/{create.json().get('id')}", timeout=5)
+    print("test_db_query deleted task \033[92mOK\033[0m" if item_delete.status_code == 200 else "\033[91mFailed\033[0m" )
+    assert item_delete.status_code == 200
--- unit_testing.py
+++ unit_testing.py
@@ -5,24 +5,29 @@ import requests
 def test_wrong_route():
     base_url = os.environ.get("TEST_BASE_URL", "http://localhost:8080")
     response = requests.get(f"{base_url}/non_existent_route", timeout=5)
+    print("test_wrong_route \033[92mOK\033[0m" if response.status_code == 404 else "\033[91mFailed\033[0m" )
     assert response.status_code == 404
 
 def test_home_route():
     base_url = os.environ.get("TEST_BASE_URL", "http://localhost:8080")
     response = requests.get(f"{base_url}/", timeout=5)
+    print("test_home_route \033[92mOK\033[0m" if response.status_code == 200 else "\033[91mFailed\033[0m" )
     assert response.status_code == 200
 
 def test_task_with_no_title():
     base_url = os.environ.get("TEST_BASE_URL", "http://localhost:8080/tasks/create",)
     response = requests.post(f"{base_url}", data={"details": "no title"}, allow_redirects=False)
+    print("test_task_with_no_title \033[92mOK\033[0m" if response.status_code == 302 else "\033[91mFailed\033[0m" )
     assert response.status_code == 302
 
 def test_reminder_with_no_title():
     base_url = os.environ.get("TEST_BASE_URL", "http://localhost:8080/reminders/create",)
     response = requests.post(f"{base_url}", data={"details": "no title"}, allow_redirects=False)
+    print("test_reminder_with_no_title \033[92mOK\033[0m" if response.status_code == 302 else "\033[91mFailed\033[0m" )
     assert response.status_code == 302
 
 def test_delete_non_existent_task():
     base_url = os.environ.get("TEST_BASE_URL", "http://localhost:8080")
     response = requests.post(f"{base_url}/tasks/delete/9999", allow_redirects=False)
+    print("test_delete_non_existent_task \033[92mOK\033[0m" if response.status_code == 404 else "\033[91mFailed\033[0m" )
     assert response.status_code == 404
